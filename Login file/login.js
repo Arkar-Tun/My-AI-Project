b1b9f955c63b764bfad073fbadf2e9a231bf86e6
Arkar-Tun (Arkar-Tun) <Arkar-Tun@users.noreply.github.com>
2026-01-18 13:45:11 +0900

    update

--- Login file/login.js
+++
@@ -1,9 +0,0 @@
-document
-  .getElementById("loginForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    alert("NexStaff မှ ကြိုဆိုပါတယ် Boss!");
-
-    window.location.href = "login.html";
-  });
