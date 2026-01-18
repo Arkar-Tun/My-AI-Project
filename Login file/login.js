05ff7e38c461dd52dcd3f2e125ee0f2c243a9d91
Arkar-Tun (Arkar-Tun) <Arkar-Tun@users.noreply.github.com>
2026-01-18 13:32:44 +0900

    update js file

--- Login file/login.js
+++ Login file/login.js
@@ -1,10 +1,9 @@
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
-    document.getElementById('loginForm').addEventListener('submit', function(event) {
-        event.preventDefault(); // Page ကို Refresh မဖြစ်အောင် တားတာပါ
-        
-        // အောင်မြင်ကြောင်း Alert ပြမယ် (ဒါက စမ်းသပ်တာပါ)
-        alert('Smart Won htan မှ ကြိုဆိုပါတယ် Boss!');
-        
-        // Dashboard ဖိုင်ဆီကို လွှတ်လိုက်မယ် (ဖိုင်နာမည် index.html ဖြစ်ရပါမယ်)
-        window.location.href = "index.html"; 
-    });
+    alert("NexStaff မှ ကြိုဆိုပါတယ် Boss!");
+
+    window.location.href = "login.html";
+  });
